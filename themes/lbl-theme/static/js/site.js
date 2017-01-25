f575b13d1f71283c2cc324f9a156e32fb209b10a
Matt Stratton <matt.stratton@gmail.com>
2017-01-25 13:46:24 -0600

    Update with theme

---
+++ themes/lbl-theme/static/js/site.js
@@ -0,0 +1,3 @@
+// @codekit-prepend '../../bower_components/jquery/dist/jquery.js'
+// @codekit-prepend '../../bower_components/tether/dist/js/tether.js'
+// @codekit-prepend '../../bower_components/bootstrap/dist/js/bootstrap.js'
